09ca41ce7aa697a70e8812304425044617745369
Pier Andrea <pierandreadelise@gmail.com>
2025-01-14 16:49:27 +0100

    first steps for turbo stream

--- lib/turbo_clone/engine.rb
+++ lib/turbo_clone/engine.rb
@@ -2,6 +2,11 @@ module TurboClone
   class Engine < ::Rails::Engine
     isolate_namespace TurboClone
 
+    # https://edgeapi.rubyonrails.org/classes/ActionController/MimeResponds.html
+    initializer "turbo.media_type" do
+      Mime::Type.register "text/vnd.turbo-stream.html", :turbo_stream
+    end
+
     # Load helpers into the main app
     # "turbo.helper" is just a name
     initializer "turbo.helper" do
