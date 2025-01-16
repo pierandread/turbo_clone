00ab134d81253b4c5fd0dd5de73a73e7f01fbe22
Pier Andrea <pierandreadelise@gmail.com>
2025-01-16 17:50:16 +0100

    replace stream acceting more types of arguments

--- app/models/turbo_clone/streams/tag_builder.rb
+++ app/models/turbo_clone/streams/tag_builder.rb
@@ -1,16 +1,28 @@
 class TurboClone::Streams::TagBuilder
   def initialize(view_context)
     @view_context = view_context
+    # ensuring that :html is included in the formats array for the render method
+    @view_context.formats |= [:html]
   end
+
+  # targer
   # <%= turbo_stream.replace @article %>
-  def replace(target)
-    action :replace, target
+  # or **rendering
+  # <%= turbo_stream.replace @article, partial: "articles/article", locals: { article: @article } %>
+  # or &block
+  # <%= turbo_stream.replace @article do %>
+  #   <%= render @article %>
+  # <% end %>
+  # or "content"
+  # <%= turbo_stream.replace @article, "Hello world" %>
+  def replace(target, content = nil, **rendering, &block)
+    action :replace, target, content, **rendering, &block
   end
 
   private
 
-  def action(name, target)
-    template = render_template(target) #  <%= render partial: "articles/article", locals: {article: @article}, formats: :html %>
+  def action(name, target, content = nil, **rendering, &block)
+    template = render_template(target, content, **rendering, &block) #  <%= render partial: "articles/article", locals: {article: @article}, formats: :html %>
 
     turbo_stream_action_tag(name, target: target, template: template)
   end
@@ -39,7 +51,16 @@ class TurboClone::Streams::TagBuilder
     end
   end
 
-  def render_template(target)
-    @view_context.render(partial: target, formats: :html)
+  def render_template(target, content = nil, **rendering, &block)
+    if content
+      content
+    elsif block_given?
+      # capture the output of a string-like output and return it as a string.
+      @view_context.capture(&block)
+    elsif rendering.any?
+        @view_context.render(**rendering, formats: :html)
+    else
+      @view_context.render(partial: target, formats: :html)
+    end
   end
 end
