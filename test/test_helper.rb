418d15afcd0a004196032c8df2b837bea381fe65
Pier Andrea <pierandreadelise@gmail.com>
2025-01-22 13:04:31 +0100

    testing all turbo stream methods thanks to show action

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,13 @@ if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
   ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures", __dir__) + "/files"
   ActiveSupport::TestCase.fixtures :all
 end
+
+module ActionViewTestCaseExtensions
+  def render(*arguments, **options, &block)
+    ApplicationController.render(*arguments, **options, &block)
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include ActionViewTestCaseExtensions
+end
