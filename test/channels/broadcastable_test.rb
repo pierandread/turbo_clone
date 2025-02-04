d60d8c8f9b61e0f436ac91485ee5ab3f7ee00a2c
Pier Andrea <pierandreadelise@gmail.com>
2025-02-04 18:17:46 +0100

    later_to tests

--- test/channels/broadcastable_test.rb
+++ test/channels/broadcastable_test.rb
@@ -2,12 +2,24 @@ require 'test_helper'
 
 class TurboClone::BroadcastableTest < ActionCable::Channel::TestCase
   include TurboClone::ActionHelper
+  # perform_enqueued_jobs
+  include ActiveJob::TestHelper
 
   test "#broadcast_append_to" do
     article = Article.create! content: "Something"
 
     assert_broadcast_on "stream_name", turbo_stream_action_tag(:append, target: "articles", template: render(article)) do
       article.broadcast_append_to "stream_name"
+    end
+  end
+
+  test "#broadcast_append_later_to" do
+    article = Article.create! content: "Something"
+
+    assert_broadcast_on "stream_name", turbo_stream_action_tag(:append, target: "articles", template: render(article)) do
+      perform_enqueued_jobs do
+        article.broadcast_append_later_to "stream_name"
+      end
     end
   end
 
@@ -19,11 +31,31 @@ class TurboClone::BroadcastableTest < ActionCable::Channel::TestCase
     end
   end
 
+  test "#broadcast_prepend_later_to" do
+    article = Article.create! content: "Something"
+
+    assert_broadcast_on "stream_name", turbo_stream_action_tag(:prepend, target: "board_articles", template: render(article)) do
+      perform_enqueued_jobs do
+        article.broadcast_prepend_later_to "stream_name", target: "board_articles"
+      end
+    end
+  end
+
   test "#broadcast_replace_to" do
     article = Article.create! content: "Something"
 
     assert_broadcast_on "stream_name", turbo_stream_action_tag(:replace, target: article, template: render(article)) do
       article.broadcast_replace_to "stream_name"
+    end
+  end
+
+  test "#broadcast_replace_later_to" do
+    article = Article.create! content: "Something"
+
+    assert_broadcast_on "stream_name", turbo_stream_action_tag(:replace, target: article, template: render(article)) do
+      perform_enqueued_jobs do
+        article.broadcast_replace_later_to "stream_name"
+      end
     end
   end
 
