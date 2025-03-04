8d2cb8e978d4b506287df2085d3bc63485a12497
Pier Andrea Delise <pierandreadelise@gmail.com>
2025-03-04 12:08:17 +0100

    Update turbo_with_importmap.rb

--- lib/install/turbo_with_importmap.rb
+++ lib/install/turbo_with_importmap.rb
@@ -2,4 +2,4 @@ say "Import Turbo"
 append_to_file "app/javascript/application.js", %(import "@hotwired/turbo-rails")
 
 say "Pin Turbo"
-run "importmap pin @hotwired/turbo-rails@7.1.1"
+run "bin/importmap pin @hotwired/turbo-rails@7.1.1"
